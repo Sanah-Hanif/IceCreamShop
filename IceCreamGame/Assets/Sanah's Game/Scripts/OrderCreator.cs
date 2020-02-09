d585ac991a9325b1cb6224cbc94c0b0b41373d0a
Sanah-Hanif <saneebanee@yahoo.com>
2020-02-09 21:50:57 +0200

    finished up the UI stuff without the pictures

--- IceCreamGame/Assets/Sanah's Game/Scripts/OrderCreator.cs
+++ IceCreamGame/Assets/Sanah's Game/Scripts/OrderCreator.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OrderCreator : MonoBehaviour
 {
     public Order[] orders;
     private Order currentOrder;
     public int ordersCompleted;
+    public TextMeshProUGUI score, nextOrder;
 
     private void Start()
     {
         ordersCompleted = 0;
         NewOrder();
+        score.text = "Score: " + ordersCompleted.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit(); // quits game
+        }
     }
 
     void NewOrder()
@@ -19,6 +31,7 @@ public class OrderCreator : MonoBehaviour
         int rand = Random.Range(0, orders.Length);
         currentOrder = orders[rand];
         print(currentOrder.name);
+        nextOrder.text = currentOrder.name.ToString();
     }
 
     public bool CheckOrder(BowlScript BS, Mixer MX)
@@ -27,6 +40,7 @@ public class OrderCreator : MonoBehaviour
         {
             print("you did it!");
             ordersCompleted++;
+            score.text = "Score: " + ordersCompleted.ToString();
             NewOrder();
             return true;
         }
